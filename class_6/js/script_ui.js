2c22b3bbce666721c65691530ba909fe427ec92e
leeseorim <leeseor12@naver.com>
2021-01-19 20:54:10 +0900

    a

--- class_6/js/script_ui.js
+++ class_6/js/script_ui.js
@@ -1,11 +1,13 @@
 $(function(){
+    var loadData; // json 데이터를 담는 변수
+    var loadDataLength;
+
     // 자리셋팅 버튼
     $(".btn_setting").click(function(){
         $(".section.box_intro").removeClass("on");
         loadDataFn();
     });
 
-    var loadData; // json 데이터를 담는 변수
     function loadDataFn(){
         $.ajax({
             url:"js/data.json", // 데이터 경로
@@ -19,6 +21,31 @@ $(function(){
 
     //자리 배치
     function settingSeatFn(){
+        //console.log(loadData.length);
         $(".section.reservation").addClass("on");
+        loadDataLength = loadData.length
+        for(var i = 0; i<loadDataLength; i++){
+            var n = loadData[i].name;
+            var p = loadData[i].price;
+            var r = loadData[i].reserve;
+        
+            $(".section.reservation > ol").append('<li class="unit"><button data-price="'+p+'" '+r+'>'+n+'</button></li>');
+        }
+        $(".section.reservation > ol button").click(function(){
+            $(this).toggleClass("select");
+            updateinfoFn();
+        });
+    }
+
+    //자리값 업데이트
+    function updateInfoFn(){
+        var selectArr =[];
+        for(var i=0; i < loadDataLength; i++){
+            if($(".section.reservation > ol button").eq(i).hasClass("select") == true){
+                selectArr.push(i);
+            }
+        }
+        console.log(selectArr);
+            
     }
 })
