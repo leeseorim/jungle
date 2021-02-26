1953423e74bdaf62daf54c42a9e1afe3d4082185
leeseorim <leeseor12@naver.com>
2021-02-27 02:05:27 +0900

    11

--- port/index/js/script_ui.js
+++ port/index/js/script_ui.js
@@ -9,15 +9,15 @@ $(function(){
     $("#section1 .inner .wrap1 .who > div").hide();
 
     $("#section1 .inner .wrap1 .who.who1 > h5> button").on("click",function(){
-        $("#section1 .inner .wrap1 .who.who1 > div").toggle("slow");
+        $("#section1 .inner .wrap1 .who.who1 > div").slideToggle("fast");
 
     });
     $("#section1 .inner .wrap1 .who.who2 > h5> button").on("click",function(){
-        $("#section1 .inner .wrap1 .who.who2 > div").toggle("slow");
+        $("#section1 .inner .wrap1 .who.who2 > div").slideToggle("fast");
 
     });
     $("#section1 .inner .wrap1 .who.who3 > h5> button").on("click",function(){
-        $("#section1 .inner .wrap1 .who.who3 > div").toggle("slow");
+        $("#section1 .inner .wrap1 .who.who3 > div").slideToggle("fast");
 
     });
 
