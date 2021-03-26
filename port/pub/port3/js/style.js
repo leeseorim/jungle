a21428fc76f4057bc027fc0986afb8b24415fde7
leeseorim <leeseor12@naver.com>
2021-03-26 11:50:14 +0900

    11

--- port/pub/port3/js/style.js
+++ port/pub/port3/js/style.js
@@ -3,7 +3,20 @@ $(function(){
     $("header .wrap .login ul .login-s > ul").hide();
 
     $("header .wrap .login ul .login-s > a").on("click",function(){
-        $("header .wrap .login ul .login-s > ul").slideToggle();
+        $("header .wrap .login ul .login-s > ul").toggle();
 
     });
+
+    $("header nav .gnb li .gnb-s").hide();
+
+    $("header nav .gnb li a").mouseenter(function(){
+        $("header nav .gnb li .gnb-s").show();
+        $("header nav").addClass("on");
+    });
+
+    $("header .nav").mouseleave(function(){
+        $("header nav .gnb li .gnb-s").hide();
+        $("header .nav.on").removeClass("on");
+    });
+    
 });
